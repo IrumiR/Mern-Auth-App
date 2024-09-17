8768f9b075ae3011223b9105f7843dc41b15318c
Irumi Rajamanthri <irumidisara@gmail.com>
2024-09-17 19:05:48 +0530

    Port changed

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World !");
@@ -13,9 +14,9 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     connectDB();
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port: ", PORT);
 });
 
 
