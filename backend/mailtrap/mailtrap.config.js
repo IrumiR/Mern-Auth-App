f0cabc14c75dee6c050ca1e53f2a642c65c27998
Irumi Rajamanthri <irumidisara@gmail.com>
2024-09-25 19:16:53 +0530

    mailtrap config file setup

---
+++ backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,14 @@
+import { MailtrapClient } from "mailtrap";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+export const MailtrapClient = new MailtrapClient({
+    endPoint: process.env.MAILTRAP_ENDPOINT,
+    token: process.env.MAILTRAP_TOKEN,
+});
+
+export const sender = {
+    email: "hello@demomailtrap.com",
+    name: "Irumi",
+};
