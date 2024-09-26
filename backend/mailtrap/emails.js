58787285630b6c08bc6265fd161a6e70b6fb4032
Irumi Rajamanthri <irumidisara@gmail.com>
2024-09-26 17:12:54 +0530

    welcome email setup

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
 import { mailtrapClient, sender } from "./mailtrap.config.js";
 
@@ -21,5 +22,26 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     console.error(`Error sending verification`, error);
 
     throw new Error(`Error sending verification email: ${error}`);
+  }
+};
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "a8fd5109-34a4-4446-bfaf-581cc4ba2376",
+      template_variables: {
+        company_info_name: "Auth Company",
+        name: name,
+      },
+    });
+    console.log("Welcome email sent successfully", response);
+  } catch (error) {
+    console.error(`Error sending welcome email`, error);
+
+    throw new Error(`Error sending welcome email: ${error}`);
   }
 };
